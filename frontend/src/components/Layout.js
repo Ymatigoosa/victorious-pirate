ee8ab13bb448e77e980c6f92b0bc222b5bd29e92
Dmitry Pahomov <topt.iiiii@gmail.com>
2016-03-29 21:25:28 +0300

    new cute menu

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -9,24 +9,33 @@ import FontIcon from 'material-ui/lib/font-icon';
 import Paper from 'material-ui/lib/paper';
 import FirebaseStore from 'stores/FirebaseStore';
 import CircularProgress from 'material-ui/lib/circular-progress';
-
-const iconStyles = {
-  marginRight: 10
-};
+import LeftNav from 'material-ui/lib/left-nav';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Divider from 'material-ui/lib/divider';
+import Subheader from 'material-ui/lib/Subheader';
 
 class Layout extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       firebaseStore: FirebaseStore,
+      isNavOpen: true,
       user: FirebaseStore.getUser()
     };
 
-    FirebaseStore.on('user-changed', (user) => {
-      this.setState({
-        user: user
-      });
-    })
+    this._onUserChange = this._onUserChange.bind(this);
+  }
+  componentWillMount() {
+    FirebaseStore.on('user-changed', this._onUserChange);
+  }
+  componentWillUnmount() {
+    FirebaseStore.off('user-changed', this._onUserChange);
+  }
+  _onUserChange (user) {
+    this.setState({
+      user: user
+    });
   }
   getCurrentTab() {
     const router = this.context.router;
@@ -48,13 +57,23 @@ class Layout extends React.Component {
 
     firebaseStore.logout(() => this.context.router.push('/'));
   }
+  toggleLeftNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
+  goToPage(page)
+  {
+    this.context.router.push(`/${page}`);
+    this.toggleLeftNav();
+  }
   render() {
     const { content, title} = this.props;
     const { user } = this.state;
     const contentWithProps = React.cloneElement(content, {
       firebaseStore: this.state.firebaseStore,
       user: this.state.user
-    })
+    });
     const rightbtn = user == null
       ? (<FlatButton
         label='Войти'
@@ -66,20 +85,38 @@ class Layout extends React.Component {
           containerElement={<Link to='/profile' />} />);
     return (
       <div>
-        <Paper zDepth={2}>
         <AppBar
           title={title}
-          iconElementLeft={<span></span>}
-          iconElementRight={rightbtn} />
-        <Tabs
+          onLeftIconButtonTouchTap={this.toggleLeftNav.bind(this)} />
+        <LeftNav
+          width={200}
+          open={this.state.isNavOpen}
+          onRequestChange={this.toggleLeftNav.bind(this)}
+          docked={false} >
+          <AppBar title="Меню" iconElementLeft={<span></span>} />
+          <List>
+            <ListItem onTouchTap={this.goToPage.bind(this, 'journal')} primaryText="Журнал" />
+            <ListItem onTouchTap={this.goToPage.bind(this, 'files')} primaryText="Файлы" />
+          </List>
+          <Divider />
+          <List>
+            <Subheader>Настройки</Subheader>
+            <ListItem onTouchTap={this.goToPage.bind(this, 'users')} primaryText="Пользователи" />
+          </List>
+          <Divider />
+          <List>
+            <Subheader>Пользователь</Subheader>
+            <ListItem primaryText="Войти" />
+          </List>
+        </LeftNav>
+        {/*<Tabs
           onChange={this.handleTabsChange.bind(this)}
           value={this.getCurrentTab.apply(this)}
           >
           <Tab value='journal' label='Журнал' />
           <Tab value='files' label='Файловый архив' />
           <Tab value='users' label='Управление пользователями' />
-        </Tabs>
-        </Paper>
+        </Tabs>*/}
         <div style={{padding: '20px'}}>{contentWithProps}</div>
       </div>
     );
