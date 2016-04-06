b3a01e651ab12194b004bbc1e9c03af816b671eb
Dmitry.Pahomov <dmitry.pahomov@technomos.ru>
2016-04-06 18:51:59 +0300

    breadcrumbs fix

--- frontend/src/components/Breadcrumbs.js
+++ frontend/src/components/Breadcrumbs.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { intersperse } from 'utils/Utils';
 
+const delimiter = ' / ';
+
 const Breadcrumbs = ({items}) => {
-  const enchantedchildren = intersperse(items, ' / ');
+  const enchantedchildren = intersperse(items, delimiter);
   return (
     <div className='Breadcrumbs'>
       {enchantedchildren.map( (el, i) => (<span key={i}>{el}</span>) )}
+      {delimiter}
     </div>
   );
 };
